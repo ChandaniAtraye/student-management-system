5e9c9a4566db7c5ca8f80883b021c96e22e3fcc9
Chandani Atraye <chandaniatraye5010@gmail.com>
2026-09-11 23:40:11 +0530

    updated app page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,8 @@ import Dashboard from "./pages/Dashboard";
 import PrivateRoute from "./routes/PrivateRoute";
 import AuthProvider from "./context/AuthContext";
 import Register from "./pages/Register";
-
+import Courses from "./pages/Courses";
+import Students from "./pages/Students";
 
 const App = () => (
   <AuthProvider>
@@ -13,7 +14,11 @@ const App = () => (
         <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
        
-          <Route path="/" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+
+        <Route path="/" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+        <Route path="/courses" element={<PrivateRoute><Courses /></PrivateRoute>} />
+ <Route path="/students" element={<PrivateRoute><Students /></PrivateRoute>} />
+     
       </Routes>
     </BrowserRouter>
   </AuthProvider>
